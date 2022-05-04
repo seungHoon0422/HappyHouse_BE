288dd8be14a659219dbb2021158bc9018d644fcd
BaekSeungHoon <psh104404@naver.com>
2022-05-04 16:48:44 +0900

    update controller

--- happyhouse_springboot/src/main/java/com/house/controller/InterestController.java
+++ happyhouse_springboot/src/main/java/com/house/controller/InterestController.java
@@ -25,17 +25,49 @@ import com.house.model.service.InterestService;
 @Controller
 @RequestMapping("/interest")
 public class InterestController {
+
+	
+	
 	@Autowired
 	private InterestService interestService;
 
+	@GetMapping("/search")	
+	@ResponseBody
+	private ResponseEntity<?> searchById(HttpSession session){
+		
+		UserDto attribute = (UserDto) session.getAttribute("userinfo");
+		String userid = attribute.getUserid();
+//		List<InterestDto> list = interestService.searchById(userid);
+//		return new ResponseEntity<List<InterestDto>>(list, HttpStatus.OK);
+		return new ResponseEntity<Void>(HttpStatus.OK); // 임시 return
+	}
+
+	
+	@GetMapping("/search/{aptCode}")
+	@ResponseBody
+	private ResponseEntity<?> searchByCode(@PathVariable int aptCode, HttpSession session){
+		System.out.println("code : "+ aptCode);
+		
+		UserDto attribute = (UserDto) session.getAttribute("userinfo");
+		String userid = attribute.getUserid();
+//		List<InterestDto> list = interestService.searchByCode(aptCode);
+//		for (InterestDto interestDto : list) {
+//			if(interestDto.getUserid().equals(userid)) {
+//				return new ResponseEntity<Integer>(1, HttpStatus.OK);
+//			}
+//		}
+		return new ResponseEntity<Integer>(0, HttpStatus.OK);
+	}
+	
+	
 	@PostMapping("/regist")
 	@ResponseBody
-
-	private ResponseEntity<?> regist(@RequestBody String aptCode, HttpSession session) throws Exception{
+	private ResponseEntity<?> regist(@RequestBody InterestDto interestDto, HttpSession session) throws Exception{
 //		System.out.println(aptCode);
 		UserDto user = (UserDto)session.getAttribute("userinfo"); // 로그인 되어있는 사람의 정보 
 		String userid = user.getUserid(); 
-		interestService.regist(userid, aptCode);
+		interestDto.setUserid(userid);
+		interestService.regist(interestDto);
 
 		// 1. session에 있는 userid받아서 interestService.regist(userid, code) 실행
 		// 2. return 0 : 등록 성공(aptCode 존재 x ), 1이상 : 이미 등록된 아파트 , 
