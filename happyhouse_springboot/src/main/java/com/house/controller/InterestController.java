3de69685ee3264d8c110e26703e55b2d3d73233c
BaekSeungHoon <psh104404@naver.com>
2022-05-04 16:34:13 +0900

    update interest controller

--- happyhouse_springboot/src/main/java/com/house/controller/InterestController.java
+++ happyhouse_springboot/src/main/java/com/house/controller/InterestController.java
@@ -1,10 +1,13 @@
 package com.house.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +26,37 @@ public class InterestController {
 
 	
 	
+//	@Autowired
+//	InterestService interestService;
+	
+	@GetMapping("/search")
+	@ResponseBody
+	private ResponseEntity<?> searchById(HttpSession session){
+		
+		UserDto attribute = (UserDto) session.getAttribute("userinfo");
+		String userid = attribute.getUserid();
+//		List<InterestDto> list = interestService.searchById(userid);
+//		return new ResponseEntity<List<InterestDto>>(list, HttpStatus.OK);
+		return new ResponseEntity<Void>(HttpStatus.OK); // 임시 return
+	}
+
+	
+	@GetMapping("/search/{aptCode}")
+	@ResponseBody
+	private ResponseEntity<?> searchByCode(@PathVariable int aptCode, HttpSession session){
+		System.out.println("code : "+ aptCode);
+		
+		UserDto attribute = (UserDto) session.getAttribute("userinfo");
+		String userid = attribute.getUserid();
+//		List<InterestDto> list = interestService.searchByCode(aptCode);
+//		for (InterestDto interestDto : list) {
+//			if(interestDto.getUserid().equals(userid)) {
+//				return new ResponseEntity<Integer>(1, HttpStatus.OK);
+//			}
+//		}
+		return new ResponseEntity<Integer>(0, HttpStatus.OK);
+	}
+	
 	
 	@PostMapping("/regist")
 	@ResponseBody
@@ -30,14 +64,11 @@ public class InterestController {
 	
 		System.out.println(interestDto.getAptCode());
 		UserDto user = (UserDto)session.getAttribute("userinfo"); // 로그인 되어있는 사람의 정보 
-		String userid = user.getUserid(); 
-		interestDto.setUserid(userid);
+		interestDto.setUserid(user.getUserid());
 		
-		System.out.println(userid);
 		
 		// 1. session에 있는 userid받아서 interestService.regist(userid, code) 실행
-		// 2. return 0 : 등록 실패(Exception), return 1 : 정상 등록, return 2 : 이미 등록된 아파트
-		return new ResponseEntity<Integer>(1, HttpStatus.OK);
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 	
 }
