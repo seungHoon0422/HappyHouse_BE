1bb0a1b9375ab0ea3d2d2b4fe49dd63551a3ca21
BaekSeungHoon <psh104404@naver.com>
2022-05-04 16:07:20 +0900

    update interest controller

--- happyhouse_springboot/src/main/java/com/house/controller/InterestController.java
+++ happyhouse_springboot/src/main/java/com/house/controller/InterestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.house.model.InterestDto;
 import com.house.model.UserDto;
 
 /***
@@ -20,15 +21,19 @@ import com.house.model.UserDto;
 @RequestMapping("/interest")
 public class InterestController {
 
+	
+	
+	
 	@PostMapping("/regist")
 	@ResponseBody
-	private ResponseEntity<?> regist(@RequestBody String aptCode, HttpSession session){
+	private ResponseEntity<?> regist(@RequestBody InterestDto interestDto, HttpSession session){
 	
-
-		System.out.println(aptCode);
-//			UserDto user = (UserDto)session.getAttribute("userinfo"); // 로그인 되어있는 사람의 정보 
-//		String userid = user.getUserid(); 
-//		System.out.println(userid);
+		System.out.println(interestDto.getAptCode());
+		UserDto user = (UserDto)session.getAttribute("userinfo"); // 로그인 되어있는 사람의 정보 
+		String userid = user.getUserid(); 
+		interestDto.setUserid(userid);
+		
+		System.out.println(userid);
 		
 		// 1. session에 있는 userid받아서 interestService.regist(userid, code) 실행
 		// 2. return 0 : 등록 실패(Exception), return 1 : 정상 등록, return 2 : 이미 등록된 아파트
