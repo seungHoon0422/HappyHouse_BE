37430509ab76f2f0453dcf435bfdaffbb7705a61
BaekSeungHoon <psh104404@naver.com>
2022-05-08 17:30:14 +0900

    update mapper annotataion

--- happyhouse_springboot/src/main/java/com/house/model/mapper/RegionMapper.java
+++ happyhouse_springboot/src/main/java/com/house/model/mapper/RegionMapper.java
@@ -2,11 +2,15 @@ package com.house.model.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.house.model.AddressDto;
 import com.house.model.DetailVo;
 import com.house.model.HouseInfoDto;
 import com.house.model.RegionDto;
 
+
+@Mapper
 public interface RegionMapper {
 	List<RegionDto> getSido();
 	List<RegionDto> getGugun();
