ed00dcd1af111af4a8596a2799e1b0b51157e84a
BaekSeungHoon <psh104404@naver.com>
2022-05-25 16:06:55 +0900

    add news crawling code

---
+++ happyhouse_springboot/src/main/java/com/house/controller/CrawlingController.java
@@ -0,0 +1,81 @@
+package com.house.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.house.model.HouseDealInfoDto;
+import com.house.model.NewsDto;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/news")
+@Api("News Crawling Controller v1")
+public class CrawlingController {
+
+	@GetMapping("/crawling")
+	public ResponseEntity<?> getNewsFeed(){
+		List<NewsDto> newsList = getStockPriceList();
+		return new ResponseEntity<List<NewsDto> >(newsList, HttpStatus.OK);
+	}
+
+	public static List<NewsDto> getStockPriceList() {
+
+	    final String stockList = "https://finance.naver.com/sise/sise_market_sum.nhn?&page=1";
+	    final String url = "http://www.hapt.co.kr/news/articleList.html?view_type=sm";
+	    Connection conn = Jsoup.connect(url);
+
+	    try {
+	      Document document = conn.get();
+	      List<NewsDto> tbody = getStockList(document);   // 데이터 리스트
+	      System.out.println(tbody);
+	      return tbody;
+
+	    } catch (IOException ignored) {
+	    	return null;
+	    }
+	}
+	public static String getStockHeader(Document document) {
+	    Elements stockTableBody = document.select("table.type_2 thead tr");
+	    StringBuilder sb = new StringBuilder();
+	    for (Element element : stockTableBody) {
+	      for (Element td : element.select("th")) {
+	        sb.append(td.text());
+	        sb.append("   ");
+	      }
+	      break;
+	    }
+	    return sb.toString();
+	  }
+
+	  public static List<NewsDto> getStockList(Document document) {
+	    Elements stockTableBody = document.select("ul.type2 li");
+	    StringBuilder sb = new StringBuilder();
+	    List<NewsDto> news = new ArrayList<NewsDto>();
+	    
+	    for (Element element : stockTableBody) {
+	    	NewsDto dto = new NewsDto();
+	    	System.out.println( "element : " + element);
+	    	dto.setTitle(element.select("li h4").text());
+	    	dto.setUrl("http://www.hapt.co.kr"+element.select("li a").attr("href"));
+	    	news.add(dto);
+	    }
+	    return news;
+	  }
+	
+}
---
+++ happyhouse_springboot/src/main/java/com/house/model/NewsDto.java
@@ -0,0 +1,32 @@
+package com.house.model;
+
+public class NewsDto {
+	String title;
+	String url;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	@Override
+	public String toString() {
+		return "NewsDto [title=" + title + ", url=" + url + "]";
+	}
+	public NewsDto(String title, String url) {
+		super();
+		this.title = title;
+		this.url = url;
+	}
+	public NewsDto() {
+		super();
+	}
+	
+	
+}
